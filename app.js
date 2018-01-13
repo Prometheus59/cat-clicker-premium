95461a8a5dde9231b588c832feecf66b5f3aa0ee
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-01-13 18:50:48 -0500

    completed octopus on app.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 
 // ************ Model ************
-var model = [
+var model = {
     currentCat: null,
     cats: [
         {
-            "name": "Cat1",
-            "pic": "https://s-media-cache-ak0.pinimg.com/736x/4c/a2/c1/4ca2c1d3ef5042461f5def25e68b2fa5.jpg",
-            "clicks": 0
+            name: "Cat1",
+            pic: "https://s-media-cache-ak0.pinimg.com/736x/4c/a2/c1/4ca2c1d3ef5042461f5def25e68b2fa5.jpg",
+            clickCount: 0
         },
         {
-            "name": "Cat2",
-            "pic": "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
-            "clicks": 0
+            name: "Cat2",
+            pic: "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
+            clickCount: 0
         },
         {
-            "name": "Cat3",
-            "pic": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTZmNMvGoUC5ypAeel-sYr4poKXmO16ykCIgkjI1rkeUaEMLpDF5Q",
-            "clicks": 0
+            name: "Cat3",
+            pic: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTZmNMvGoUC5ypAeel-sYr4poKXmO16ykCIgkjI1rkeUaEMLpDF5Q",
+            clickCount: 0
         },
         {
-            "name": "Cat4",
-            "pic": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-1_ibjpaTeqpOxtlN0ToWI7kO73oWjq6gcbqXsGMe3i_MZOtl",
-            "clicks": 0
+            name: "Cat4",
+            pic: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-1_ibjpaTeqpOxtlN0ToWI7kO73oWjq6gcbqXsGMe3i_MZOtl",
+            clickCount: 0
         },
         {
-            "name": "Cat5",
-            "pic": "https://www.aspca.org/sites/default/files/cat-care_cat-nutrition-tips_overweight_body4_left.jpg",
-            "clicks": 0
+            name: "Cat5",
+            pic: "https://www.aspca.org/sites/default/files/cat-care_cat-nutrition-tips_overweight_body4_left.jpg",
+            clicks: 0
         }
     ]
-];
+};
 
 
 
@@ -95,12 +95,31 @@ var catListView = {
 
 // *********** Octopus Start **************
 
-function viewCat(id){
+var octopus = {
+    
+    init: function(){
+        model.currentCat = model.cats[0];
 
-}
+        catListView.init();
+        catView.init();
+    },
 
-$(document).ready(function () {
+    getCurrentCat: function(){
+        return model.currentCat;
+    },
 
-}
-);
+    getCats: function(){
+        return model.cats;
+    },
 
+    setCurrentCat: function(cat){
+        model.currentCat = cat;
+    },
+
+    incrementCounter: function(){
+        model.currentCat.clickCount++;
+        catView.render;
+    }
+};
+
+octopus.init();
